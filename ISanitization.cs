ee337bcc73294b212b4eddbf5a1d3297bdf72ea7
cauasaguirre <cauasaguirre@gmail.com>
2026-05-07 19:48:05 -0300

    Cauã Santos Aguirre: Adiciona interface ISanitization para sanitização de entrada

---
+++ ISanitization.cs
@@ -0,0 +1,10 @@
+namespace OOPFoundation
+{
+    /// <summary>
+    /// Interface para sanitização de texto.
+    /// </summary>
+    public interface ISanitization
+    {
+        string Sanitize(string textToSanitize);
+    }
+}

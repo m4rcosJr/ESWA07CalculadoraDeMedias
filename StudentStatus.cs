71aa4cceda3a5836d673f3a0c01330c5fcca7e37
Marcos Junior <marcosfsjr10@gmail.com>
2026-05-07 15:39:43 -0300

    Create StudentStatus.cs

---
+++ StudentStatus.cs
@@ -0,0 +1,13 @@
+namespace ESWA07CalculadoraDeMedias
+{
+    /// <summary>
+    /// Status do aluno após o cálculo das médias.
+    /// </summary>
+    public enum StudentStatus
+    {
+        EmAndamento,
+        Aprovado,
+        EmExame,
+        Reprovado
+    }
+}
